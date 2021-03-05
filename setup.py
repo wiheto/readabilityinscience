e8e8314d96fef08b8c49878719ab9dc63b232cf2
Russell Jarvis <russelljarvis@protonmail.com>
2021-03-05 12:54:18 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-# old functional:
 from distutils.core import setup
 import setuptools
-
 import os
-
-# try:
 from setuptools import setup
-
-# except ImportError:
-# from distutils.core import setup, find_packages
-
 
 def read_requirements():
     """parses requirements from requirements.txt"""
